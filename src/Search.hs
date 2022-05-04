b0fe20c9a7ecbed5edbdb2b808742b5f4e2e9480
Claude Zou <claude.zou.work@gmail.com>
2022-05-04 15:49:13 -0400

    adjust move ordering

--- src/Search.hs
+++ src/Search.hs
@@ -15,19 +15,22 @@ searchPosition state@(GameState _ col _ _ _ _ _ _ _) = case generateMoves state 
     -- should refactor because it calls alphabeta multiple times on each element...
 
 alphaBeta :: GameState -> Int -> Int -> Int -> Int
+alphaBeta state _ _ (-1) = evalPosition state
 alphaBeta state _ _ 0 = evalPosition state
-alphaBeta state@(GameState _ White _ _ _ _ _ _ _) a b depth = helperF (generateMoves state) a b depth (-10000000)
+alphaBeta state@(GameState _ White _ _ _ _ _ _ _) a b depth = helperF (f $ generateMoves state) a b depth (-10000000)
   where
         helperF [] _ _ _ _ = if isPlayerInCheck state White then -9999999-depth else 0 -- never called recursively, only when states is empty
         helperF [x] a b depth val = max val (alphaBeta x a b (depth-1))
         helperF (x:xs) a b depth val = if newval >= b then newval else helperF xs (max a newval) b depth newval
           where newval = max val (alphaBeta x a b (depth-1))
-alphaBeta state@(GameState _ Black _ _ _ _ _ _ _) a b depth = helperF (generateMoves state) a b depth 10000000
+        f l = map (\x -> snd x) $ sortBy (\x y -> compare (fst y) (fst x)) (map (\x -> (evalPosition x, x)) l)
+alphaBeta state@(GameState _ Black _ _ _ _ _ _ _) a b depth = helperF (f $ generateMoves state) a b depth 10000000
   where
         helperF [] _ _ _ _ = if isPlayerInCheck state Black then 9999999+depth else 0
         helperF [x] a b depth val = min val (alphaBeta x a b (depth-1))
         helperF (x:xs) a b depth val = if newval <= a then newval else helperF xs a (min b newval) depth newval
           where newval = min val (alphaBeta x a b (depth-1))
+        f l = map (\x -> snd x) $ sortBy (\x y -> compare (fst x) (fst y)) (map (\x -> (evalPosition x, x)) l)
 
 minimax :: GameState -> Int -> Int
 minimax state 0 = evalPosition state
